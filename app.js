ad55c846013f769783834b76a20676e3906a1461
dilshad360 <dilshad360.dev@gmail.com>
2024-06-12 11:50:31 +0530

    test

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.get('/api/getnews', async (req, res) => {
 });
 
 
+app.get('/api/getnews', async (req, res) => {
+    res.status(201).json("working");
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
