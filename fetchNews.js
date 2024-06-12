605a08c8895931ad3a5f505d8b8fea15a573967b
dilshad360 <dilshad360.dev@gmail.com>
2024-06-12 15:10:26 +0530

    rm: cron

--- fetchNews.js
+++ fetchNews.js
@@ -1,47 +1,37 @@
-
-const axios = require('axios');
-const { newsArrayTemp }  = require('./const.js');
-const News   = require('./models/news.js');
-
+const axios = require("axios");
+const News = require("./models/news.js");
 
 const NEWS_API_KEY = process.env.NEWS_API_KEY;
 
 async function fetchAndStoreNews() {
-
-
     try {
-        
-        // const response = await axios.get(`https://newsdata.io/api/1/news?language=en&country=in&category=technology&apikey=${NEWS_API_KEY}`);
-        
-        // const newsArray = response.data.results
-        const newsArray = newsArrayTemp;
-        
+        const response = await axios.get(
+            `https://newsdata.io/api/1/news?language=en&country=in&category=technology&apikey=${NEWS_API_KEY}`
+        );
+
+        const newsArray = response.data.results;
+
         dropNewsCollection();
 
         await News.insertMany(newsArray);
 
-
-        console.log('News fetched and stored successfully.');
+        console.log("News fetched and stored successfully.");
     } catch (error) {
-        console.error('Error fetching news:', error);
+        console.error("Error fetching news:", error);
     }
 }
 
 async function dropNewsCollection() {
-    
     try {
         await News.collection.drop();
-        console.log('News collection dropped.');
+        console.log("News collection dropped.");
     } catch (error) {
         if (error.code === 26) {
-            console.log('News collection does not exist.');
+            console.log("News collection does not exist.");
         } else {
             throw error;
         }
     }
 }
-
-
-
 
 module.exports = fetchAndStoreNews;
--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const News = require('./models/news.js');
-
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const News = require("./models/news.js");
+const fetchAndStoreNews = require("./fetchNews.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
+console.log(process.env.MONGO_URI);
 
-console.log(process.env.MONGO_URI)
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB", err);
+    });
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB', err);
-});
-
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
     res.status(201).json("api server working");
 });
 
-
-app.get('/api/getnews', async (req, res) => {
+app.get("/api/getnews", async (req, res) => {
     try {
         const data = await News.find();
-        if(data.length===0 ){
-            return res.status(404).json({message:"No news found"})
+        if (data.length === 0) {
+            return res.status(404).json({ message: "No news found" });
         }
         res.status(201).json(data);
     } catch (err) {
@@ -37,11 +38,15 @@ app.get('/api/getnews', async (req, res) => {
     }
 });
 
-
+app.get("/api/updatenews", async (req, res) => {
+    try {
+        await fetchAndStoreNews();
+        res.status(201).json("News Updated");
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
-
-
