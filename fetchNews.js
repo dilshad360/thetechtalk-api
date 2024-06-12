b9ec623376c723b11aa1a1edfd76735f7e7a2657
dilshad360 <dilshad360.dev@gmail.com>
2024-06-12 12:09:06 +0530

    testing temp data

--- fetchNews.js
+++ fetchNews.js
@@ -12,12 +12,12 @@ async function fetchAndStoreNews() {
 
     try {
         
-        const response = await axios.get(`https://newsdata.io/api/1/news?language=en&country=in&category=technology&apikey=${NEWS_API_KEY}`);
+        // const response = await axios.get(`https://newsdata.io/api/1/news?language=en&country=in&category=technology&apikey=${NEWS_API_KEY}`);
         
-        const newsArray = response.data.results
-        // const newsArray = newsArrayTemp;
+        // const newsArray = response.data.results
+        const newsArray = newsArrayTemp;
         
-        await dropNewsCollection();
+        dropNewsCollection();
 
         await News.insertMany(newsArray);
 
